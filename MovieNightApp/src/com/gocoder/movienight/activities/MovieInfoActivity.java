cd5e4ca662592df68ef68f71922d989fa3dc7986
sumitusc <sumitgupt@gmail.com>
2014-02-22 22:59:54 -0800

    Refactored MovieIntent and manifest file

--- MovieNightApp/src/com/gocoder/movienight/activities/MovieInfoActivity.java
+++ MovieNightApp/src/com/gocoder/movienight/activities/MovieInfoActivity.java
@@ -3,6 +3,8 @@ package com.gocoder.movienight.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.gocoder.movienight.R;
@@ -12,7 +14,7 @@ import com.squareup.picasso.Picasso;
 /**
  * Created by ashishn on 2/21/14.
  */
-public class MovieIntent extends Activity {
+public class MovieInfoActivity extends Activity {
 
     MovieModel movie;
 
@@ -22,6 +24,7 @@ public class MovieIntent extends Activity {
 
 
     public void onCreate(Bundle savedInstanceState) {
+    	Log.d("DEBUG","Movie Info");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.individual_movie);
         Intent i = getIntent();
@@ -29,6 +32,7 @@ public class MovieIntent extends Activity {
         movieImage = (ImageView) findViewById(R.id.movieImage);
         movieImage.setScaleType(ImageView.ScaleType.FIT_START);
         description = (TextView) findViewById(R.id.description);
+        Log.d("DEBUG","Movie Info - process");
         process();
     }
 
